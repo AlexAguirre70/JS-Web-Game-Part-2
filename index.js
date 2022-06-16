4c4a1be76cf3b82bc06c7a27c3e3d95fd3adadb9
Alex Aguirre <teamaguirre@gmail.com>
2022-06-16 08:27:07 -0700

    Add scope to variables to make them accessible across functions

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ function newImage(url, left, bottom){
     return object
 }
 
+
 function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+    let item = newImage(url, left, bottom);
+    item.addEventListener('click', function() {
+        console.log(item)
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src=url
+        inventory.append(inventoryItem)
+    })
+
 }
 
+let inventory;
 function newInventory(){
-    let inventory = document.createElement('div')
+    inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
